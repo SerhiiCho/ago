9fb4bfd032387ea4f4e401e0772f49427357fd81
Wojtczyk <4815785+awojtczyk@users.noreply.github.com>
2020-07-13 19:07:40 +0200

    Create pl.php
    PL lang.

---
+++ src/lang/pl.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+return [
+
+    'ago' => 'temu',
+    'online' => 'Online',
+    // Seconds
+    'second' => 'sekundę',
+    'seconds' => 'sekund',
+    'seconds2' => 'sekund',
+    // Minutes
+    'minute' => 'minutę',
+    'minutes' => 'minut',
+    'minutes2' => 'minut',
+    // Hours
+    'hour' => 'godzinę',
+    'hours' => 'godzin',
+    'hours2' => 'godzin',
+    // Days
+    'day' => 'dzień',
+    'days' => 'dni',
+    'days2' => 'dni',
+    // Weeks
+    'week' => 'tydzień',
+    'weeks' => 'tygodni',
+    'weeks2' => 'tygodni',
+    // Months
+    'month' => 'miesiąc',
+    'months' => 'miesięcy',
+    'months2' => 'miesięcy',
+    // Years
+    'year' => 'rok',
+    'years' => 'lat',
+    'years2' => 'lat',
+
+];
