f832c2018564738d4a532b27e95ca00777e8be85
SerhiiCho <serhiicho@protonmail.com>
2021-12-05 16:56:05 +0200

    Changed comments for rules en and ru

--- resources/rules/en.php
+++ resources/rules/en.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 /**
- * @param int $number The number of seconds, minutes, hours, days, weeks, months and years.
- * @param int $last_digit Last digit of the number, if number with 1 digit return it.
+ * @param int $number The number that represents seconds, minutes, hours, days, weeks, months and years.
+ * If input is 1 year, this number will be 1, if input is 5 minutes this number will be 5.
+ * @param int $last_digit Last digit of the number. For example 23 will be 3, 2 will be 2, 60 will be 0.
  *
  * @return bool[]|array[]
  */
