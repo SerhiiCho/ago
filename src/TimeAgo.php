c289c402faf804c1fe616019d5ec4958c636fbc0
SerhiiCho <serhiicho@protonmail.com>
2024-09-08 17:30:35 +0300

    Small code refactoring

--- src/TimeAgo.php
+++ src/TimeAgo.php
@@ -207,7 +207,8 @@ final class TimeAgo
         }
 
         if ($this->optionIsSet(Option::JUST_NOW) && $this->optionIsSet(Option::ONLINE)) {
-            throw new InvalidOptionsException('Option JUST_NOW and ONLINE are incompatible. Use only one of them');
+            $msg = 'Option JUST_NOW and ONLINE are incompatible. Use only one of them';
+            throw new InvalidOptionsException($msg);
         }
     }
 }
